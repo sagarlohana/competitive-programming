d43bc808b95b415d7a0d02e04ff556afa65dc67d
Sagar Lohana <sagar_lohana@hotmail.com>
2019-08-26 22:43:57 -0400

    Added 1x easy

---
+++ LeetCode/peakindex.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for i in range (len(A)-2):
+            if A[i] < A[i+1] and A[i+1] > A[i+2]:
+                return i+1
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        return A.index(max(A))
